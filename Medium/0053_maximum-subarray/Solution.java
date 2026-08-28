8cafcaecda4f6b4cd1e90bd1be018a5cd9427bd0
Test-robot-0 <muli.24bcy10254@vitbhopal.ac.in>
2026-08-28 20:54:45 +0530

    Added Solution for 0053_maximum-subarray

---
+++ Medium/0053_maximum-subarray/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++){
+
+            if (currentSum + nums[i] > nums[i]){
+                currentSum += nums[i];
+            } else {
+                currentSum = nums[i];
+            }
+
+            if (maxSum < currentSum){
+                maxSum = currentSum;
+            }
+        }
+
+        return maxSum;
+    }
+}
