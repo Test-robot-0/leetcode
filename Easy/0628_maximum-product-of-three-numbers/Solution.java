2780c8696f53db9f5256faf496c79b3a2804062b
Test-robot-0 <muli.24bcy10254@vitbhopal.ac.in>
2026-08-28 21:10:33 +0530

    Added Solution for 0628_maximum-product-of-three-numbers

---
+++ Easy/0628_maximum-product-of-three-numbers/Solution.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+class Solution {
+    public int maximumProduct(int[] nums) {
+
+        Arrays.sort(nums);
+
+        System.out.println(Arrays.toString(nums));
+
+        int product1 = nums[nums.length-1] * nums[nums.length-2] * nums[nums.length-3];
+        int product2 = nums[0] * nums[1] * nums[nums.length-1];
+
+
+        System.out.println(product1);
+        System.out.println(product2);
+
+        if (product1 > product2){
+            return product1;
+        } else {
+            return product2;
+        }
+
+    }
+}
