5183dbc3b3ea42e6ac3babdefbb3b0c15d23c0c9
Test-robot-0 <muli.24bcy10254@vitbhopal.ac.in>
2026-08-28 21:14:17 +0530

    Added Solution for 0645_set-mismatch

---
+++ Easy/0645_set-mismatch/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+
+        int i = 0;
+        while (i < nums.length){
+            int correct = nums[i]-1;
+
+            if (nums[correct] != nums[i]){
+                int temp = nums[correct];
+                nums[correct] = nums[i];
+                nums[i] = temp;
+            } else {
+                i++;
+            }
+        }
+
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j+1){
+                return new int[]{nums[j], j+1};
+            }
+        }
+
+        return new int[]{1, 1};
+
+    }
+}
