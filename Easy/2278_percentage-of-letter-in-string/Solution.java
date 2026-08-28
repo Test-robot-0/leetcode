b569f79fea54149a25b173e639b3bd3a3df10075
Test-robot-0 <muli.24bcy10254@vitbhopal.ac.in>
2026-08-28 21:16:52 +0530

    Added Solution for 2278_percentage-of-letter-in-string

---
+++ Easy/2278_percentage-of-letter-in-string/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int percentageLetter(String s, char letter) {
+
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++){
+            char x = s.charAt(i);
+
+            if (x == letter){
+                count++;
+            }
+        }
+
+        return (count * 100) / s.length();
+        
+    }
+}
