07e4607a51a3269053728bd333f7a57a01184e29
Test-robot-0 <muli.24bcy10254@vitbhopal.ac.in>
2026-09-07 00:48:12 +0530

    Added javascript Solution for 0169_majority-element

---
+++ Easy/0169_majority-element/Solution.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let candidate = nums[0];
+    let count = 0;
+
+    for (const num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+
+        if (candidate === num) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+
+    return candidate;
+};
