546fed6621fb6fdb71b9a897aa5231e4d4e5a4a0
Test-robot-0 <muli.24bcy10254@vitbhopal.ac.in>
2026-09-07 00:26:41 +0530

    Added typescript Solution for 3903_smallest-stable-index-i

---
+++ Easy/3903_smallest-stable-index-i/Solution.ts
@@ -0,0 +1,20 @@
+function firstStableIndex(nums: number[], k: number): number {
+    const n = nums.length;
+    const suffixMin: number[] = new Array(n);
+    suffixMin[n - 1] = nums[n - 1];
+
+    for (let i = n - 2; i >= 0; i--) {
+        suffixMin[i] = Math.min(nums[i], suffixMin[i + 1]);
+    }
+
+    let prefixMax = nums[0];
+    for (let i = 0; i < n; i++) {
+        prefixMax = Math.max(prefixMax, nums[i]);
+        const instability = prefixMax - suffixMin[i];
+        if (instability <= k) {
+            return i;
+        }
+    }
+
+    return -1;
+}
