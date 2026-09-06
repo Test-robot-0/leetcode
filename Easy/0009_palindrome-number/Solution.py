6c52a152611d8dfc0f7ead68898878f9616829be
Test-robot-0 <muli.24bcy10254@vitbhopal.ac.in>
2026-09-07 01:22:26 +0530

    Added python Solution for 0009_palindrome-number

---
+++ Easy/0009_palindrome-number/Solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        num = x
+        reversed_num = 0
+        if x >= 0:
+            while x != 0:
+                digit = x % 10
+                reversed_num = reversed_num * 10 + digit
+                x //= 10
+            return num == reversed_num
+        else:
+            return False
