a2187c733911f0d3b65ea9bc450c52f9ad5a0ab8
Test-robot-0 <muli.24bcy10254@vitbhopal.ac.in>
2026-09-07 00:26:33 +0530

    Added python Solution for 3903_smallest-stable-index-i

---
+++ Easy/3903_smallest-stable-index-i/Solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def firstStableIndex(self, nums: list[int], k: int) -> int:
+        n = len(nums)
+        suffixMin = [0] * n
+        suffixMin[n - 1] = nums[n - 1]
+        
+        for i in range(n - 2, -1, -1):
+            suffixMin[i] = min(nums[i], suffixMin[i + 1])
+            
+        prefixMax = nums[0]
+        for i in range(n):
+            prefixMax = max(prefixMax, nums[i])
+            instability = prefixMax - suffixMin[i]
+            if instability <= k:
+                return i
+                
+        return -1
