9ec88c22badd3230be8609fb76b4ac3896e155aa
Test-robot-0 <muli.24bcy10254@vitbhopal.ac.in>
2026-09-07 00:48:11 +0530

    Added cpp Solution for 0169_majority-element

---
+++ Easy/0169_majority-element/Solution.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+
+class Solution {
+public:
+    int majorityElement(std::vector<int>& nums) {
+        int candidate = nums[0];
+        int count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (candidate == num) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+};
