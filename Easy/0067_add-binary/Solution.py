9506628425ef7ef6f50685169c693199df2aa565
Test-robot-0 <muli.24bcy10254@vitbhopal.ac.in>
2026-09-07 21:47:19 +0530

    Added python Solution for 0067_add-binary

---
+++ Easy/0067_add-binary/Solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        result = []
+        i = len(a) - 1
+        j = len(b) - 1
+        carry = 0
+        while i >= 0 or j >= 0 or carry != 0:
+            sum_val = carry
+            if i >= 0:
+                sum_val += int(a[i])
+                i -= 1
+            if j >= 0:
+                sum_val += int(b[j])
+                j -= 1
+            result.append(str(sum_val % 2))
+            carry = sum_val // 2
+        return "".join(reversed(result))
