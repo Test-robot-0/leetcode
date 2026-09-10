dcf481a68288a2082b940fb8de973c0559257df3
Test-robot-0 <muli.24bcy10254@vitbhopal.ac.in>
2026-09-10 19:18:27 +0530

    Added cpp Solution for 0080_remove-duplicates-from-sorted-array-ii

---
+++ Medium/0080_remove-duplicates-from-sorted-array-ii/Solution.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+
+class Solution {
+public:
+    int removeDuplicates(std::vector<int>& nums) {
+        if (nums.size() <= 2) return nums.size();
+        int slow = 2;
+        for (int fast = 2; fast < nums.size(); fast++) {
+            if (nums[fast] != nums[slow - 2]) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        return slow;
+    }
+};
