bfd3d258690e768f655d3e746aa4f75586161b2c
Test-robot-0 <muli.24bcy10254@vitbhopal.ac.in>
2026-09-10 19:17:07 +0530

    Added typescript Solution for 0167_two-sum-ii-input-array-is-sorted

---
+++ Medium/0167_two-sum-ii-input-array-is-sorted/Solution.ts
@@ -0,0 +1,15 @@
+function twoSum(numbers: number[], target: number): number[] {
+    let first = 0;
+    let last = numbers.length - 1;
+    while (first < last) {
+        let sum = numbers[first] + numbers[last];
+        if (sum > target) {
+            last--;
+        } else if (sum < target) {
+            first++;
+        } else {
+            return [first + 1, last + 1];
+        }
+    }
+    return [-1, -1];
+}
