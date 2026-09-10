0af8944ff06c013181184bf275d6d7fa775a3b09
Test-robot-0 <muli.24bcy10254@vitbhopal.ac.in>
2026-09-10 19:18:28 +0530

    Added javascript Solution for 0080_remove-duplicates-from-sorted-array-ii

---
+++ Medium/0080_remove-duplicates-from-sorted-array-ii/Solution.js
@@ -0,0 +1,11 @@
+var removeDuplicates = function(nums) {
+    if (nums.length <= 2) return nums.length;
+    let slow = 2;
+    for (let fast = 2; fast < nums.length; fast++) {
+        if (nums[fast] !== nums[slow - 2]) {
+            nums[slow] = nums[fast];
+            slow++;
+        }
+    }
+    return slow;
+};
